506d9ce98390be36974d82d8c2adc926800f618a
marktennenhouse <marktennenhouse@hotmail.com>
2019-07-18 18:55:12 -0400

    4 files modified

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Input } from '@angular/core';
-
+import { Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-product-alert',
   templateUrl: './product-alert.component.html',
@@ -10,6 +10,8 @@ export class ProductAlertComponent implements OnInit {
 
   constructor() { }
   @Input() product;
+  @Output() notify = new EventEmitter();
+
 
   ngOnInit() {
   }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+
+  OnNotify(){
+    window.alert("You will be notified of the price change");
+  }
 }
 
 
