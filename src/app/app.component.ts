5a031aeac9d9960b28bd6cd2259895b0da6c45c4
marktennenhouse <marktennenhouse@hotmail.com>
2019-07-18 17:31:09 -0400

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent  {}
 
 
 /*
+This is the master branch.
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
